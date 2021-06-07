5a84eb27eca393e5ffa73d8a38014ad469b2e10d
Aakash Prasad <73053809+aakash-prasad@users.noreply.github.com>
2021-06-07 12:07:27 +0530

    Final

--- routes/customers.js
+++ routes/customers.js
@@ -2,8 +2,21 @@ const express = require('express');
 const router = express.Router();
 const {conn} = require('../connection');
 
+router.get('/', async(req,res)=>{
+  // START THE CONNECTION
+  const mySqlConnection = await conn();
+  // QUERY TO GET CUSTOMER
+  const getCustomerQuery = 'SELECT * FROM customers';
+  const getCustomerResult = await mySqlConnection.execute(getCustomerQuery);
+  let customerArray = getCustomerResult[0]
+  console.log(customerArray)
+  //RETURN THE ARRAY
+  return res.status(200).json({data: customerArray})  
+});
+
 
 router.post('/', async(req,res)=>{
+  //START THE CONNECTION
   const mySqlConnection = await conn();
   
   const{userName, phoneNo, gender} =req.body;
@@ -16,99 +29,99 @@ router.post('/', async(req,res)=>{
   //check weather the userName exist or not:
   const checkUserQuery = 'SELECT username FROM customers WHERE username = ?';
   const checkUserResult = await mySqlConnection.execute(checkUserQuery, [userName]);
-  
   // If userName doesn't exist insert new customer:
   if(checkUserResult[0].length==0){
       const insertQuery = 'INSERT INTO customers(username, phone, gender) VALUES(?,?,?)';
       const insertResult = await mySqlConnection.execute(insertQuery, customerDetails)
+      return res.status(200).json({data: customerDetails});
   }
   else{
     console.log('User Already exist')
-    return res.json({msg: 'User Already Exist'})
+    return res.status(409).json({msg: 'User Already Exist'})
   }  
-  }catch(err){console.log(`Error in Creating a new customer: ${err}`)}
-  
-  return res.json({customerDetails});
+  }catch(err){return res.status(500).json({err})}
 })
+  
 
-router.get('/:username', async(req, res)=>{
+
+router.get('/:id', async(req, res)=>{
   const mySqlConnection = await  conn();
-  const userName  = (req.params.username)
-  console.log(userName)
+  const userId  = (req.params.id)
+  console.log(userId)
   try{
-  //SEARACH FOR THE USER WITH THE USERNAME IN THE DATABASE
-  const getUserQuery = 'SELECT * FROM customers WHERE username = ?';
-  const getUserResult = await mySqlConnection.execute(getUserQuery, [userName])
+  //SEARACH FOR THE USER WITH THE id IN THE DATABASE
+  const getUserQuery = 'SELECT * FROM customers WHERE id = ?';
+  const getUserResult = await mySqlConnection.execute(getUserQuery, [userId])
   const userData = (getUserResult[0][0])
   console.log(userData)
+  return res.status(200).json({data :userData})
+  }catch(err){return res.status(500).json({err})}
+
+})
+
+
+router.post('/:id', async(req, res)=>{
+  const mySqlConnection = await  conn();
+  const userId  = (req.params.id)
+  
+  const newData  = [userName, phoneNo]
+  // UPDATE THE DETAILS WITH THE GIVEN USER ID
+  const updateQuery = 'UPDATE customers SET username = ?,  phone = ? WHERE id = ?';
+  const updateQueryResult = await mySqlConnection.execute(updateQuery, [userName, phoneNo, userId])
+  return res.json({msg:'ok'})
+})
+
+
+
+router.get('/:id/books', async(req, res)=>{
+  const mySqlConnection = await  conn();
+  const userId  = (req.params.id)
+  try{
+  //SEARACH FOR THE USER WITH THE id IN THE DATABASE
+  const getUserQuery = 'SELECT * FROM customers WHERE id = ?';
+  const getUserResult = await mySqlConnection.execute(getUserQuery, [userId])
+  const userData = (getUserResult[0][0])
 
   //Search for the book issued by the customer and push it into the userData object
   const customerId  = getUserResult[0][0].id;
   const getBookIdQuery = 'SELECT book_id FROM issue_book WHERE customer_id = ?';
   const getBookIdResult = await mySqlConnection.execute(getBookIdQuery, [customerId])
-  const bookId = (getBookIdResult[0][0].book_id);
-  const getBookQuery = 'SELECT name FROM books WHERE id = ?';
-  const getBookResult = await mySqlConnection.execute(getBookQuery, [bookId]);
-  const bookName = (getBookResult[0][0].name)
-  userData.bookName = bookName;
+  //IF BOOK IS THERE FOR USER THEN SEARCH FOR IT
+  if(getBookIdResult[0].length != 0){
+    const bookArray = [];
+  getBookIdResult.forEach(async(item,index)=>{
+    const bookId = (getBookIdResult[0][index].book_id);
+    console.log(getBookIdResult[0][0])
   
-  //IF USER FOUND RESPOND WITH THE USER DETAILS
-  res.json({Customer :userData})
-  }catch(err){console.log(`Error in getting the user ${err}`)}
+    const getBookQuery = 'SELECT name FROM books WHERE id = ?';
+    const getBookResult = await mySqlConnection.execute(getBookQuery, [bookId]);
+  
+    const bookName = (getBookResult[0][0].name)
+    console.log(getBookResult[0][0].name)
+    bookArray.push(bookName)
+  })
+  userData.bookName = bookArray;
+  return res.status(200).json({data :userData})
+  }
+  else{return res.status(200).json({data :userData}) }
+  }catch(err){return res.status(500).json({err})}
 
 })
 
 
-router.post('/update', async(req, res)=>{
-  const {userName, phoneNo} = req.body;
+
+router.post('/:id1/book/:id2', async(req, res)=>{
   const mySqlConnection = await conn();
-  
-  //CHECK IF USERNAME EXIST OR NOT
-  const checkUserQuery = 'SELECT username FROM customers WHERE username = ?';
-  const checkUserResult = await mySqlConnection.execute(checkUserQuery, [userName]);
-  //IF PHONE NOT VALID
-  if(phoneNo.length !=10){
-    return res.json({msg: 'Enter a valid Phone Number'})
-  }
-
-  //IF USERNAME EXIST THEN UPDATE THE NUMBER
-  if(checkUserResult[0].length == 0){
-    return res.json({msg: 'USER NOT FOUND'})
-  }
-
-  //ELSE RESPOND WITH 'USER NOT FOUND'
-  const updatePhoneQuery = 'UPDATE customers SET phone = ? WHERE username = ?';
-  const updatePhoneResult = await mySqlConnection.execute(updatePhoneQuery, [phoneNo, userName])
-  return res.json({phoneNo})
-})
-
-
-router.post('/all', async(req,res)=>{
-  const mySqlConnection = await conn();
-
-  // GET ALL THE CUSTOMERS FROM THE DATABASE WITH THEIR BOOKS ISSUED
-  const getCustomerQuery = 'SELECT * FROM customers';
-  const getCustomerResult = await mySqlConnection.execute(getCustomerQuery);
-  let customerArray = getCustomerResult[0]
-  //console.log(customerArray)
-  customerArray.forEach(async(item,index)=>{
-  const customerId = customerArray[index].id;
-  const getBookIdQuery = 'SELECT book_id FROM issue_book WHERE customer_id = ?'
-  const getBookIdResult =  await mySqlConnection.execute(getBookIdQuery, [customerId])
-  
-  if(getBookIdResult[0].length !=0){
-    const bookId = (getBookIdResult[0][0].book_id);
-    //console.log(bookId)
-    const getBookNameQuery = 'SELECT name FROM books WHERE id = ?'
-    const getBookNameResult = await mySqlConnection.execute(getBookNameQuery, [bookId])
-    let bookName = (getBookNameResult[0][0].name)
-    //console.log(bookName)
-    const userObject = (customerArray[index])
-    console.log(userObject)
-    userObject.book = bookName;
-    return res.json({allCustomer: customerArray})
-    }
-  });
+  const customerId =req.params.id1;
+  const bookId =req.params.id2;
+  console.log(customerId)
+  console.log(bookId)
+  // issue the book with bookId with customerId
+  try{
+    const issueQuery = 'INSERT INTO issue_book(customer_id, book_id, issue_date) VALUES(?,?,?)';
+    const issueResult = await mySqlConnection.execute(issueQuery, [customerId, bookId, new Date()])
+    return res.status(200).json({data: [customerId, bookId, new Date()]})
+  }catch(err){return res.status(500).json({err})}
   
 })
 
